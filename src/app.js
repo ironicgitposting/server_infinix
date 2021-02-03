740692b52e96095c9d8983752e701bd43c7433cd
ironicgitposting <patrickxavierrichard@gmail.com>
2021-02-03 15:16:53 +0100

    corr cors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-
-const corsMiddleware = require('./middlewares/cors/cors.middleware');
 const userRoutes = require('./routes/users.route');
+
 
 const app = express();
 
 const API_VERSION = '/api/v1';
 
 // Middlewares
-app.use(corsMiddleware({
-  allowedMethods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
