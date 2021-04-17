c9049b30d5822cd33783d4ad6648650e84984207
romain <romain98.bedjai@gmail.com>
2021-04-17 15:24:50 +0200

    models sequelize

--- src/models/status.js
+++ src/models/status.js
@@ -1,25 +1,17 @@
-'use strict';
-const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class status extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      status.hasMany(models.familyStatus);
-    }
+  const status = sequelize.define("status", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    label: DataTypes.STRING,
+  });
+
+  status.associate = (models) => {
+    status.hasOne(models.familyStatus);
   };
-  status.init(
-    {
-      label: DataTypes.STRING,
-      familyStatus: DataTypes.INTEGER
-    },
-    {
-      sequelize,
-      modelName: 'Status',
-    },
-  );
+
   return status;
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,50 +1,34 @@
-const { Model, STRING, TEXT } = require('sequelize');
-const { commonModel, commonOptions} = require('')
-
-// npx sequelize-cli model:generate
-// --name User --attributes firstName:string,lastName:string,
-// email:string
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      User.hasMany(models.status);
-      User.hasMany(models.civility);
-    }
+  const user = sequelize.define("user", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    registrationNumber: DataTypes.STRING,
+    surname: DataTypes.STRING,
+    name: DataTypes.STRING,
+    profession: DataTypes.INTEGER,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    telephone: DataTypes.STRING,
+    authorizationAccess: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+    dateLastSeen: DataTypes.DATE,
+    site: DataTypes.INTEGER,
+    language: DataTypes.INTEGER,
+    archived: DataTypes.BOOLEAN,
+    archivedDate: DataTypes.DATE,
+    flagChangePassword: DataTypes.BOOLEAN,
+    flagDurablePassword: DataTypes.BOOLEAN,
+  });
+
+  user.associate = (models) => {
+    user.hasOne(models.status);
+    user.hasOne(models.civility);
   };
-  User.init(
-    {
-      registrationNumber: DataTypes.STRING,
-      surname: DataTypes.STRING,
-      name: DataTypes.STRING,
-      profession: DataTypes.INTEGER,
-      password: DataTypes.STRING,
-      email: {
-        type: DataTypes.STRING,
-        unique: true
-      },
-      telephone: DataTypes.STRING,
-      authorizationAccess: DataTypes.INTEGER,
-      createdAt: DataTypes.DATE,
-      updatedAt: DataTypes.DATE,
-      dateLastSeen: DataTypes.DATE,
-      site: DataTypes.INTEGER,
-      language: DataTypes.INTEGER,
-      archived: DataTypes.BOOLEAN,
-      archivedDate: DataTypes.DATE,
-      flagChangePassword: DataTypes.BOOLEAN,
-      flagDurablePassword: DataTypes.BOOLEAN,
-      status: DataTypes.INTEGER,
-      civility: DataTypes.INTEGER
-    },
-    {
-      sequelize,
-      modelName: 'User',
-    },
-  );
-  return User;
+
+  return user;
 };
