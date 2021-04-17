27ef06cd7fddbd6439c5985cc516a121960242dc
Corentin <corentin.jules2018@campus-eni.fr>
2021-04-17 15:27:51 +0200

    G3-146 Partie Réservation du véhicule: Back
    Routes booking

---
+++ src/migrations/20210417131353-create-booking.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      driver: {
+        type: Sequelize.INTEGER
+      },
+      vehicule: {
+        type: Sequelize.INTEGER
+      },
+      startDate: {
+        type: Sequelize.DATE
+      },
+      endDate: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      departureSite: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Bookings');
+  }
+};
---
+++ src/models/booking.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Booking.init({
+    driver: DataTypes.INTEGER,
+    vehicule: DataTypes.INTEGER,
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE,
+    status: DataTypes.INTEGER,
+    departureSite: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Booking',
+  });
+  return Booking;
+};
