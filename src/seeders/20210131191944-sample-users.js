48662c7e7b706f1e461cecd1425a45b07422af9e
romain <romain98.bedjai@gmail.com>
2021-04-27 14:46:59 +0200

    seeds user

--- src/seeders/20210131191944-sample-users.js
+++ src/seeders/20210131191944-sample-users.js
@@ -1,22 +1,16 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert(
-    'Users',
-    [{
-        firstName: 'Kobe',
-        lastName: 'Bryant',
-        email: 'kobe@blackmamba.com',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.bulkInsert("Users", [
       {
-        firstName: 'Big',
-        lastName: 'Shaq',
-        email: 'shaq@bigcactus.com',
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        surname: "admin",
+        name: "admin",
+        password: "admin",
+        email: "infinix.supp@gmail.com",
+        telephone: "0000000000",
+        authorizationAccess: 1,
       },
-    ],
-  ),
+    ]),
 
-  down: async (queryInterface, Sequelize) => queryInterface.bulkDelete('Users', null, {}),
+  down: async (queryInterface, Sequelize) =>
+    queryInterface.bulkDelete("Users", null, {}),
 };
