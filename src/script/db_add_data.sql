727660a5ee53edf892c645a2d86a0d44d7056bd8
Snezki <romain.menard2018@campus-eni.fr>
2021-04-30 09:54:25 +0200

    ajout du script création

---
+++ src/script/db_add_data.sql
@@ -0,0 +1,28 @@
+INSERT INTO public."Site"(id, label, adress, "postalCode", city, phone, mail, pays, "createdAt", "updatedAt", status) 
+VALUES (1, 'saint herblain', '2 rue de france', '44000', 'Nantes', '0251676352', null, null, now(), now(), null);
+
+
+INSERT INTO public."Vehicules"(id, type, libelle, model, "flagService", "createdAt", "updatedAt", immatriculation, state, status, site)     
+VALUES (1, 1, 'auto', 'tesla', true, now(), now(), 'UI-777-SA', null, null, null);
+
+INSERT INTO public."Vehicules"(id, type, libelle, model, "flagService", "createdAt", "updatedAt", immatriculation, state, status, site)     
+VALUES (2, 1, 'auto', '208', true, now(), now(), 'AA-123-FF', null, null, null);
+
+INSERT INTO public."Vehicules"(id, type, libelle, model, "flagService", "createdAt", "updatedAt", immatriculation, state, status, site)     
+VALUES (3, 1, 'auto', 'A3', true, now(), now(), 'CV-458-RT', null, null, null);
+
+
+INSERT INTO public."Status"(id, label, "createdAt", "updatedAt", "familyStatus")     
+VALUES (1, 'en attente de validation', now(), now(), null);
+
+INSERT INTO public."Status"(id, label, "createdAt", "updatedAt", "familyStatus")     
+VALUES (2, 'en cours', now(), now(), null);
+
+INSERT INTO public."Status"(id, label, "createdAt", "updatedAt", "familyStatus")     
+VALUES (3, 'cloturé', now(), now(), null);
+
+INSERT INTO public."Status"(id, label, "createdAt", "updatedAt", "familyStatus")     
+VALUES (4, 'validé', now(), now(), null);
+
+INSERT INTO public."Status"(id, label, "createdAt", "updatedAt", "familyStatus")     
+VALUES (5, 'en retard', now(), now(), null);
