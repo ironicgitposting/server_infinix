203a032774f4af1babe30049fc518fe62a97aafb
Patrick <prichard@herculepro.com>
2021-06-22 13:16:13 +0200

    corr routes settings

--- src/routes/settings.route.js
+++ src/routes/settings.route.js
@@ -1,13 +1,14 @@
 const express = require('express');
+
 const checkAuth = require('../middleware/checkAuth');
 const SettingsController = require('../controllers/settings.controller');
 
 const router = express.Router();
 
 // Get All Settings for user of id :id
-router.get('/settings', checkAuth, SettingsController.getSettings);
+router.get('', checkAuth, SettingsController.getSettings);
 
 // Update Setting
-router.get('/settings/update/:id', checkAuth, SettingsController.updateSetting);
+router.get('/update/:id', checkAuth, SettingsController.updateSetting);
 
 module.exports = router;
