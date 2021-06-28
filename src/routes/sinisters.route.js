877a4901bee6f7c3424e5015987252759d84ef01
Snezki <romain.menard2018@campus-eni.fr>
2021-06-28 16:48:40 +0200

    checkauth pour les routes sinistres

--- src/routes/sinisters.route.js
+++ src/routes/sinisters.route.js
@@ -6,15 +6,15 @@ const router = express.Router();
 
 
 // Get list of sinisters or just one sinister 
-router.get("/:idVehicle", SinisterConstroller.getSinisters);
+router.get("/:idVehicle",checkAuth, SinisterConstroller.getSinisters);
 
 // Create sinister
-router.post("/create", SinisterConstroller.createSinister);
+router.post("/create", checkAuth, SinisterConstroller.createSinister);
 
 // Update sinister
-router.post("/update/:id", SinisterConstroller.updateSinister);
+router.post("/update/:id", checkAuth, SinisterConstroller.updateSinister);
 
 // Archive sinister
-router.get("/delete/:id", SinisterConstroller.deleteSinister);
+router.get("/delete/:id", checkAuth, SinisterConstroller.deleteSinister);
 
 module.exports = router;
