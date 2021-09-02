1f020409669c62e96fb332acab0d2e969f5dbe1a
Corentin <corentin.j.jules@gmail.com>
2021-09-02 11:38:44 +0200

    Seeds Status et FamilyStatus

---
+++ src/seeders/20210902082214-status-seeder.js
@@ -0,0 +1,87 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) =>
+        queryInterface.bulkInsert("Status", [
+            {
+                id: 1,
+                label: "En attente de validation",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 2
+            },
+            {
+                id: 2,
+                label: "En cours",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 2
+            },
+            {
+                id: 3,
+                label: "Clôturé",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 2
+            },
+            {
+                id: 4,
+                label: "Validé",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 2
+            },
+            {
+                id: 5,
+                label: "En retard",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 2
+            },
+            {
+                id: 6,
+                label: "Annulé",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 2
+            },
+            {
+                id: 1000,
+                label: "Site Ouvert",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 2
+            },
+            {
+                id: 2000,
+                label: "Site Fermé",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 2
+            },
+            {
+                id: 100,
+                label: "Découvert",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 3
+            },
+            {
+                id: 200,
+                label: "Pris en charge",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 3
+            },
+            {
+                id: 300,
+                label: "Résolu",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                familyStatus: 3
+            },
+        ]),
+
+    down: async (queryInterface, Sequelize) =>
+        queryInterface.bulkDelete("Status", null, {})
+};
